c980c7378ca048701b832842918a11474493ce62
AbdManian <abd.manian@gmail.com>
2022-05-08 01:55:54 +0430

    submit leap year

--- leap/lib/year.ex
+++ leap/lib/year.ex
@@ -9,6 +9,11 @@ defmodule Year do
       unless the year is also evenly divisible by 400
   """
   @spec leap_year?(non_neg_integer) :: boolean
-  def leap_year?(year) do
-  end
+  def leap_year?(year) when rem(year, 400) == 0, do: true
+  
+  @spec leap_year?(non_neg_integer) :: boolean
+  def leap_year?(year) when rem(year, 100) == 0, do: false
+
+  @spec leap_year?(non_neg_integer) :: boolean
+  def leap_year?(year), do: rem(year, 4) == 0
 end
