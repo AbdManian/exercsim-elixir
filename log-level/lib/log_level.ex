2bf30db10056444ff434de330d6cba342281b827
AbdManian <abd.manian@gmail.com>
2022-05-08 00:27:06 +0430

    refactor to use tuple

--- log-level/lib/log_level.ex
+++ log-level/lib/log_level.ex
@@ -1,29 +1,24 @@
 defmodule LogLevel do
-  def legacy_check(:trace, true), do: :unknown
-  def legacy_check(:fatal, true), do: :unknown
-  def legacy_check(label, _), do: label
-
   def to_label(level, legacy?) do
-    case level do
-      0 -> :trace
-      1 -> :debug
-      2 -> :info
-      3 -> :warning
-      4 -> :error
-      5 -> :fatal
-      _ -> :unknown
+    case {level, legacy?} do
+      {0, false} -> :trace
+      {1, _} -> :debug
+      {2, _} -> :info
+      {3, _} -> :warning
+      {4, _} -> :error
+      {5, false} -> :fatal
+      {_, _} -> :unknown
     end
-    |> legacy_check(legacy?)
   end
 
   def alert_recipient(level, legacy?) do
-    label = to_label(level, legacy?)
-
-    cond do
-      label == :fatal or label == :error -> :ops
-      label == :unknown and legacy? -> :dev1
-      label == :unknown -> :dev2
-      true -> false
+    case {to_label(level, legacy?), legacy?} do
+      {:fatal, _} -> :ops
+      {:error, _} -> :ops
+      {:unknown, true} -> :dev1
+      {:unknown, false} -> :dev2
+      _ -> false
     end
   end
+
 end
