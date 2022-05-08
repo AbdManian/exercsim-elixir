493a43db70d1c253773f577218b27d55d2ccaf97
AbdManian <abd.manian@gmail.com>
2022-05-09 00:24:14 +0430

    refactor to use IF statement

--- name-badge/lib/name_badge.ex
+++ name-badge/lib/name_badge.ex
@@ -1,13 +1,8 @@
 defmodule NameBadge do
-
-  defp dep_name(nil), do: "OWNER"
-  defp dep_name(name), do: String.upcase(name)
-
-  defp id_str(nil), do: ""
-  defp id_str(id), do: "[#{id}] - "
-
-
   def print(id, name, department) do
-    "#{id_str(id)}#{name} - #{dep_name(department)}"
+    id_str = if id, do: "[#{id}] - ", else: ""
+    dep_str = if department, do: String.upcase(department), else: "OWNER"
+
+    id_str <> "#{name} - #{dep_str}"
   end
 end
