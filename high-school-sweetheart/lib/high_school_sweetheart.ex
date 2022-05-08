0439727cc1ac6f92f134793d9addebab525ed24c
Abd Manian <abd.manian@gmail.com>
2022-05-08 17:13:24 +0430

    Refactor code for pair/2

--- high-school-sweetheart/lib/high_school_sweetheart.ex
+++ high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -9,27 +9,33 @@ defmodule HighSchoolSweetheart do
 
   def initials(full_name) do
     full_name
-    |> String.trim
-    |> String.split
+    |> String.trim()
+    |> String.split()
     |> Enum.map(&initial/1)
     |> Enum.join(" ")
   end
 
   def pair(full_name1, full_name2) do
-"     ******       ******
-   **      **   **      **
- **         ** **         **
-**            *            **
-**                         **
-**     #{initials full_name1}  +  #{initials full_name2}     **
- **                       **
-   **                   **
-     **               **
-       **           **
-         **       **
-           **   **
-             ***
-              *
-"
+    {n1, n2} = {
+      initials(full_name1),
+      initials(full_name2)
+    }
+
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{n1}  +  #{n2}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
   end
 end
