6b61ea71e909558faec61edd44fd7642b628605a
AbdManian <abd.manian@gmail.com>
2022-05-10 21:07:09 +0430

    Exercise answer is added

--- collatz-conjecture/lib/collatz_conjecture.ex
+++ collatz-conjecture/lib/collatz_conjecture.ex
@@ -1,4 +1,5 @@
 defmodule CollatzConjecture do
+  require Integer
   @doc """
   calc/1 takes an integer and returns the number of steps required to get the
   number to 1 when following the rules:
@@ -6,6 +7,9 @@ defmodule CollatzConjecture do
     - if number is even, divide by 2
   """
   @spec calc(input :: pos_integer()) :: non_neg_integer()
-  def calc(input) do
-  end
+  def calc(1), do: 0
+
+  def calc(x) when x > 0 and Integer.is_even(x), do: 1 + calc(div(x, 2))
+
+  def calc(x) when x > 0 and Integer.is_odd(x), do: 1 + calc(3 * x + 1)
 end
