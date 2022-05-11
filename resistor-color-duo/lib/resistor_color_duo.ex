b0038dd3f90af4ad3807850d2d0e65b427461394
Abd Manian <abd.manian@gmail.com>
2022-05-11 16:50:14 +0430

    Initial solution

--- resistor-color-duo/lib/resistor_color_duo.ex
+++ resistor-color-duo/lib/resistor_color_duo.ex
@@ -1,8 +1,25 @@
 defmodule ResistorColorDuo do
+  @color_table %{
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9
+  }
+
   @doc """
   Calculate a resistance value from two colors
   """
   @spec value(colors :: [atom]) :: integer
-  def value(colors) do
+
+  def value([first, second]) do
+    @color_table[first] * 10 + @color_table[second]
   end
+
+  def value([first, second, _]), do: value([first, second])
 end
