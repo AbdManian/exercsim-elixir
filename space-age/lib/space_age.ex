98062b3a9cc88923fb1f0988f93c258e9dc25a67
Abd Manian <abd.manian@gmail.com>
2022-05-12 11:23:23 +0430

    Space year solution

--- space-age/lib/space_age.ex
+++ space-age/lib/space_age.ex
@@ -1,4 +1,17 @@
 defmodule SpaceAge do
+  @planets_year %{
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1.0,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
+
+  @earth_year_seconds 31_557_600
+
   @type planet ::
           :mercury
           | :venus
@@ -15,5 +28,9 @@ defmodule SpaceAge do
   """
   @spec age_on(planet, pos_integer) :: {:ok, float} | {:error, String.t()}
   def age_on(planet, seconds) do
+    case @planets_year[planet] do
+      nil -> {:error, "not a planet"}
+      earth_year_ratio -> {:ok, seconds / @earth_year_seconds / earth_year_ratio}
+    end
   end
 end
