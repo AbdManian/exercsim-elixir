94b28afb6021d6c8563cafe047e72bd6aa40da46
AbdManian <abd.manian@gmail.com>
2022-05-13 00:06:04 +0430

    Initial submit

--- binary-search/lib/binary_search.ex
+++ binary-search/lib/binary_search.ex
@@ -19,5 +19,28 @@ defmodule BinarySearch do
 
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
   def search(numbers, key) do
+    binary_search(numbers, 0, tuple_size(numbers) - 1, key)
+  end
+
+  defp binary_search({}, _, _, _), do: :not_found
+
+  defp binary_search(numbers, start_index, end_index, key)
+       when start_index == end_index or start_index + 1 == end_index do
+    cond do
+      elem(numbers, start_index) == key -> {:ok, start_index}
+      elem(numbers, end_index) == key -> {:ok, end_index}
+      true -> :not_found
+    end
+  end
+
+  defp binary_search(numbers, start_index, end_index, key) do
+    mid = div(start_index + end_index, 2)
+    mid_val = elem(numbers, mid)
+
+    cond do
+      mid_val == key -> {:ok, mid}
+      mid_val > key -> binary_search(numbers, start_index, mid, key)
+      true -> binary_search(numbers, mid, end_index, key)
+    end
   end
 end
