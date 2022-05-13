228c33ff90fdeab8f1985967ce964c5c188aa903
AbdManian <abd.manian@gmail.com>
2022-05-13 23:28:06 +0430

    submit solution

--- take-a-number/lib/take_a_number.ex
+++ take-a-number/lib/take_a_number.ex
@@ -1,5 +1,23 @@
 defmodule TakeANumber do
   def start() do
-    # Please implement the start/0 function
+    spawn(fn -> machine(0) end)
+  end
+
+  defp machine(state) do
+    receive do
+      {:report_state, sender_pid} ->
+        send(sender_pid, state)
+        machine(state)
+
+      {:take_a_number, sender_pid} ->
+        send(sender_pid, state + 1)
+        machine(state + 1)
+
+      :stop ->
+        :ok
+
+      _ ->
+        machine(state)
+    end
   end
 end
