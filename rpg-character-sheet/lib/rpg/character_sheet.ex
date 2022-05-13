174bd43858c8f0cef515a1cf38c8b9be15c01ade
AbdManian <abd.manian@gmail.com>
2022-05-13 23:01:37 +0430

    Submit refactord solution

--- rpg-character-sheet/lib/rpg/character_sheet.ex
+++ rpg-character-sheet/lib/rpg/character_sheet.ex
@@ -1,21 +1,33 @@
 defmodule RPG.CharacterSheet do
   def welcome() do
-    # Please implement the welcome/0 function
+    IO.puts("Welcome! Let's fill out your character sheet together.")
+  end
+
+  defp get_user_input(message) do
+    IO.gets(message) |> String.trim()
   end
 
   def ask_name() do
-    # Please implement the ask_name/0 function
+    get_user_input("What is your character's name?\n")
   end
 
   def ask_class() do
-    # Please implement the ask_class/0 function
+    get_user_input("What is your character's class?\n")
   end
 
   def ask_level() do
-    # Please implement the ask_level/0 function
+    get_user_input("What is your character's level?\n")
+    |> String.to_integer()
   end
 
   def run() do
-    # Please implement the run/0 function
+    welcome()
+
+    %{
+      name: ask_name(),
+      class: ask_class(),
+      level: ask_level()
+    }
+    |> IO.inspect(label: "Your character")
   end
 end
