8d275d127fa676eb546d4f60229e7324407a333a
Abd Manian <abd.manian@gmail.com>
2022-05-15 19:59:27 +0430

    Incomplete solution

--- dna-encoding/lib/dna.ex
+++ dna-encoding/lib/dna.ex
@@ -1,14 +1,40 @@
 defmodule DNA do
   def encode_nucleotide(code_point) do
-    # Please implement the encode_nucleotide/1 function
+    case code_point do
+      ?\s -> 0b0000
+      ?A -> 0b0001
+      ?C -> 0b0010
+      ?G -> 0b0100
+      ?T -> 0b1000
+    end
   end
 
   def decode_nucleotide(encoded_code) do
-    # Please implement the decode_nucleotide/1 function
+    case encoded_code do
+      0b0000 -> ?\s
+      0b0001 -> ?A
+      0b0010 -> ?C
+      0b0100 -> ?G
+      0b1000 -> ?T
+    end
   end
 
   def encode(dna) do
-    # Please implement the encode/1 function
+    # Enum.each(
+    #   dna,
+    #   &(IO.puts "V>>>>>>>>>> Val=#{encode_nucleotide(&1)}")
+    # )
+
+    #
+    Enum.reduce(
+      dna,
+      << >>,
+      fn c, acc -> IO.puts ">>>>>>>> #{}"
+
+
+
+      #&(<<&2, encode_nucleotide(&1)::size(4)>>)
+    )
   end
 
   def decode(dna) do
