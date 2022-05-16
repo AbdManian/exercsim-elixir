98b58db1e8895adec9bdef2929fc719fcf554e4e
AbdManian <abd.manian@gmail.com>
2022-05-16 23:54:53 +0430

    Add solution

--- basketball-website/lib/basketball_website.ex
+++ basketball-website/lib/basketball_website.ex
@@ -1,9 +1,13 @@
 defmodule BasketballWebsite do
-  def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
-  end
+  def extract_from_path(data, path), do: get_in_path(data, path)
+
+  defp extract_access([], data), do: data
+
+  defp extract_access([x | rest], data), do: extract_access(rest, data[x])
 
   def get_in_path(data, path) do
-    # Please implement the get_in_path/2 function
+    path
+    |> String.split(".")
+    |> extract_access(data)
   end
 end
