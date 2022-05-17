aa138b1c4cd547496527f7de96fbd7cf1978b561
AbdManian <abd.manian@gmail.com>
2022-05-18 01:26:14 +0430

    Use single map for file/type/magic

--- file-sniffer/lib/file_sniffer.ex
+++ file-sniffer/lib/file_sniffer.ex
@@ -1,25 +1,20 @@
 defmodule FileSniffer do
   @file_types %{
-    "exe" => "application/octet-stream",
-    "bmp" => "image/bmp",
-    "png" => "image/png",
-    "jpg" => "image/jpg",
-    "gif" => "image/gif"
+    "exe" => {"application/octet-stream", <<0x7F, 0x45, 0x4C, 0x46>>},
+    "bmp" => {"image/bmp", <<0x42, 0x4D>>},
+    "png" => {"image/png", <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>},
+    "jpg" => {"image/jpg", <<0xFF, 0xD8, 0xFF>>},
+    "gif" => {"image/gif", <<0x47, 0x49, 0x46>>}
   }
 
-  @file_magic %{
-    "exe" => <<0x7F, 0x45, 0x4C, 0x46>>,
-    "bmp" => <<0x42, 0x4D>>,
-    "png" => <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>,
-    "jpg" => <<0xFF, 0xD8, 0xFF>>,
-    "gif" => <<0x47, 0x49, 0x46>>
-  }
-
-  def type_from_extension(extension), do: @file_types[extension]
+  def type_from_extension(extension) do
+    {type, _} = @file_types[extension]
+    type
+  end
 
   defp ext_from_binary(file_binary) do
     {ext, _} =
-      Enum.find(@file_magic, fn {_, pattern} -> String.starts_with?(file_binary, pattern) end)
+      Enum.find(@file_types, fn {_, {_, pattern}} -> String.starts_with?(file_binary, pattern) end)
 
     ext
   end
