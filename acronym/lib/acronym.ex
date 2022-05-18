0827cc13a2c688148cd2227c2e099653b22b6c76
AbdManian <abd.manian@gmail.com>
2022-05-18 07:51:55 +0430

    initial solution

--- acronym/lib/acronym.ex
+++ acronym/lib/acronym.ex
@@ -5,5 +5,13 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
+    string
+    |> String.upcase()
+    |> String.replace("-", " ")
+    |> String.replace("_", " ")
+    |> String.split()
+    |> Enum.map(&String.codepoints/1)
+    |> Enum.map(&hd/1)
+    |> Enum.join("")
   end
 end
