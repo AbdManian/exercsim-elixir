41652f3a52dc686bad13b2a0ca9caea681ce909c
AbdManian <abd.manian@gmail.com>
2022-05-18 22:26:18 +0430

    Submit solution (first iteration)

--- newsletter/lib/newsletter.ex
+++ newsletter/lib/newsletter.ex
@@ -1,21 +1,22 @@
 defmodule Newsletter do
-  def read_emails(path) do
-    # Please implement the read_emails/1 function
-  end
+  def read_emails(path), do: File.read!(path) |> String.split()
 
-  def open_log(path) do
-    # Please implement the open_log/1 function
-  end
+  def open_log(path), do: File.open!(path, [:write])
 
-  def log_sent_email(pid, email) do
-    # Please implement the log_sent_email/2 function
-  end
+  def log_sent_email(pid, email), do: IO.write(pid, "#{email}\n")
 
-  def close_log(pid) do
-    # Please implement the close_log/1 function
-  end
+  def close_log(pid), do: File.close(pid)
 
   def send_newsletter(emails_path, log_path, send_fun) do
-    # Please implement the send_newsletter/3 function
+    logf = open_log(log_path)
+
+    read_emails(emails_path)
+    |> Enum.each(fn email_addr ->
+      if send_fun.(email_addr) == :ok do
+        log_sent_email(logf, email_addr)
+      end
+    end)
+
+    close_log(logf)
   end
 end
