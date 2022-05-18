f099b40bd1eb119dd44bcd42949e68b35433abd2
Abd Manian <abd.manian@gmail.com>
2022-05-18 15:31:56 +0430

    Initial solution

--- anagram/lib/anagram.ex
+++ anagram/lib/anagram.ex
@@ -4,5 +4,19 @@ defmodule Anagram do
   """
   @spec match(String.t(), [String.t()]) :: [String.t()]
   def match(base, candidates) do
+    candidates
+    |> Enum.filter(&anagram?(base, &1))
+  end
+
+  defp anagram?(base, word) do
+    base = String.upcase(base)
+    word = String.upcase(word)
+
+    if base == word do
+      false
+    else
+      base |> String.codepoints() |> Enum.sort() ==
+        word |> String.codepoints() |> Enum.sort()
+    end
   end
 end
