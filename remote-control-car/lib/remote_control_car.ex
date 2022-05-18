e60b7150223b959f7b71decc08fc2a1b2ce984a5
AbdManian <abd.manian@gmail.com>
2022-05-19 00:34:39 +0430

    Refactor using __module__ as self reference

--- remote-control-car/lib/remote_control_car.ex
+++ remote-control-car/lib/remote_control_car.ex
@@ -2,20 +2,19 @@ defmodule RemoteControlCar do
   @enforce_keys [:nickname]
   defstruct battery_percentage: 100, distance_driven_in_meters: 0, nickname: nil
 
-  def new(nickname \\ "none"), do: %RemoteControlCar{nickname: nickname}
+  def new(nickname \\ "none"), do: %__MODULE__{nickname: nickname}
 
-  @spec display_distance(%RemoteControlCar{}) :: String.t()
-  def display_distance(%RemoteControlCar{distance_driven_in_meters: miter}), do: "#{miter} meters"
+  def display_distance(%__MODULE__{distance_driven_in_meters: miter}), do: "#{miter} meters"
 
-  def display_battery(%RemoteControlCar{battery_percentage: 0}), do: "Battery empty"
+  def display_battery(%__MODULE__{battery_percentage: 0}), do: "Battery empty"
 
-  def display_battery(%RemoteControlCar{battery_percentage: battery_value}),
+  def display_battery(%__MODULE__{battery_percentage: battery_value}),
     do: "Battery at #{battery_value}%"
 
-  def drive(remote_car = %RemoteControlCar{battery_percentage: 0}), do: remote_car
+  def drive(remote_car = %__MODULE__{battery_percentage: 0}), do: remote_car
 
   def drive(
-        remote_car = %RemoteControlCar{
+        remote_car = %__MODULE__{
           battery_percentage: battery,
           distance_driven_in_meters: distance
         }
