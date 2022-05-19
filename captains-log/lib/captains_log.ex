c35cfbed23b90da07fe1ff72ec3d0db944718915
AbdManian <abd.manian@gmail.com>
2022-05-19 23:53:03 +0430

    Fix based on analyzer comments

--- captains-log/lib/captains_log.ex
+++ captains-log/lib/captains_log.ex
@@ -3,11 +3,12 @@ defmodule CaptainsLog do
 
   def random_planet_class(), do: @planetary_classes |> Enum.random()
 
-  def random_ship_registry_number(), do: "NCC-#{999 + :rand.uniform(9000)}"
+  def random_ship_registry_number(), do: "NCC-#{Enum.random(1000..9999)}"
 
   def random_stardate(), do: 41000.0 + 1000.0 * :rand.uniform()
 
-  def format_stardate(stardate) when is_float(stardate), do: "#{round(stardate * 10) / 10.0}"
+  def format_stardate(stardate) when is_float(stardate),
+    do: :io_lib.format("~.1f", [stardate]) |> List.to_string()
 
   def format_stardate(_), do: raise(ArgumentError, "Grefti ma ro?")
 end
