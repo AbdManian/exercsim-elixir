f58a40b2cc23a7583224f5ca7b3c85caa78e8f36
AbdManian <abd.manian@gmail.com>
2022-05-19 23:04:22 +0430

    Initial import

--- bread-and-potions/lib/rpg.ex
+++ bread-and-potions/lib/rpg.ex
@@ -20,4 +20,32 @@ defmodule RPG do
   end
 
   # Add code to define the protocol and its implementations below here...
+  defprotocol Edible do
+    def eat(item, character)
+  end
+
+  defimpl Edible, for: LoafOfBread do
+    @bread_health_inc 5
+    def eat(_item, character) do
+      {nil, %Character{character | health: character.health + @bread_health_inc}}
+    end
+  end
+
+  defimpl Edible, for: ManaPotion do
+    def eat(%ManaPotion{strength: mana_value}, char) do
+      {
+        %EmptyBottle{},
+        Map.update(char, :mana, mana_value, &(&1 + mana_value))
+      }
+    end
+  end
+
+  defimpl Edible, for: Poison do
+    def eat(_, char) do
+      {
+        %EmptyBottle{},
+        %Character{char | health: 0}
+      }
+    end
+  end
 end
