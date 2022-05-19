322dc14c25e26ba94cdbbe41bf93bc6ec2d93579
Abd Manian <abd.manian@gmail.com>
2022-05-19 18:21:30 +0430

    Initial solution

--- community-garden/lib/community_garden.ex
+++ community-garden/lib/community_garden.ex
@@ -5,23 +5,49 @@ defmodule Plot do
 end
 
 defmodule CommunityGarden do
-  def start(opts) do
-    # Please implement the start/1 function
+  def start(_opts \\ []) do
+    Agent.start(fn ->
+      %{
+        last_plot_id: 0,
+        plot_list: []
+      }
+    end)
   end
 
   def list_registrations(pid) do
-    # Please implement the list_registrations/1 function
+    Agent.get(pid, fn %{plot_list: plot_list} -> plot_list end)
+  end
+
+  defp get_last_plot_id(pid) do
+    Agent.get(pid, fn x -> x.last_plot_id end)
   end
 
   def register(pid, register_to) do
-    # Please implement the register/2 function
+    plot_id = get_last_plot_id(pid) + 1
+
+    new_plot = %Plot{plot_id: plot_id, registered_to: register_to}
+
+    Agent.update(pid, fn x ->
+      %{
+        last_plot_id: plot_id,
+        plot_list: x.plot_list ++ [new_plot]
+      }
+    end)
+
+    new_plot
   end
 
+  defp drop_plot_id(plot_id_list, rmove_id),
+    do: Enum.filter(plot_id_list, fn x -> x.plot_id != rmove_id end)
+
+  defp find_by_plot_id(plot_id_list, target_id),
+    do: Enum.find(plot_id_list, {:not_found, "plot is unregistered"}, &(&1.plot_id == target_id))
+
   def release(pid, plot_id) do
-    # Please implement the release/2 function
+    Agent.update(pid, fn x -> %{x | plot_list: drop_plot_id(x.plot_list, plot_id)} end)
   end
 
   def get_registration(pid, plot_id) do
-    # Please implement the get_registration/2 function
+    Agent.get(pid, fn %{plot_list: plot_list} -> find_by_plot_id(plot_list, plot_id) end)
   end
 end
