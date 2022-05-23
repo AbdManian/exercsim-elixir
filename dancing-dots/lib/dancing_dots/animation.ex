d4b9b468325ec2a16c758571e8a96753cef3d53b
AbdManian <abd.manian@gmail.com>
2022-05-23 23:41:21 +0430

    Initial solution

--- dancing-dots/lib/dancing_dots/animation.ex
+++ dancing-dots/lib/dancing_dots/animation.ex
@@ -4,13 +4,51 @@ defmodule DancingDots.Animation do
   @type error :: any
   @type frame_number :: pos_integer
 
-  # Please implement the module
+  @callback init(opts) :: {:ok, opts} | {:error, error}
+
+  @callback handle_frame(dot(), pos_integer(), opts()) :: dot()
+
+  defmacro __using__(_) do
+    quote do
+      @behaviour DancingDots.Animation
+      def init(opts), do: {:ok, opts}
+      defoverridable init: 1
+    end
+  end
 end
 
 defmodule DancingDots.Flicker do
-  # Please implement the module
+  # @behaviour DancingDots.Animation
+  use DancingDots.Animation
+
+  @impl DancingDots.Animation
+  def handle_frame(dot, pos_integer, _) do
+    if rem(pos_integer, 4) == 0 do
+      %{dot | opacity: dot.opacity / 2}
+    else
+      dot
+    end
+  end
 end
 
 defmodule DancingDots.Zoom do
+  @behaviour DancingDots.Animation
+
+  @impl true
+  def init(opts) do
+    if Keyword.has_key?(opts, :velocity) and is_number(opts[:velocity]) do
+      {:ok, opts}
+    else
+      s = if is_nil(opts[:velocity]), do: "nil", else: "\"#{opts[:velocity]}\""
+
+      {:error, "The :velocity option is required, and its value must be a number. Got: #{s}"}
+    end
+  end
+
+  @impl DancingDots.Animation
+  def handle_frame(dot, pos_integer, opts) do
+    %{dot | radius: dot.radius + (pos_integer - 1) * opts[:velocity]}
+  end
+
   # Please implement the module
 end
