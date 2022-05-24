c4484bb9beb4455fc45fc6ae7e6449afa99dcd0e
Abd Manian <abd.manian@gmail.com>
2022-05-24 19:52:33 +0430

    Half the solution with help of community solutions

--- rpn-calculator-inspection/lib/rpn_calculator_inspection.ex
+++ rpn-calculator-inspection/lib/rpn_calculator_inspection.ex
@@ -1,10 +1,20 @@
 defmodule RPNCalculatorInspection do
+
   def start_reliability_check(calculator, input) do
-    # Please implement the start_reliability_check/2 function
+    {:ok, pid} = Task.start_link(fn ->calculator.(input) end)
+    %{input: input, pid: pid}
   end
 
   def await_reliability_check_result(%{pid: pid, input: input}, results) do
-    # Please implement the await_reliability_check_result/2 function
+    reply =
+      receive do
+        {:EXIT, ^pid, :normal} -> :ok
+        {:EXIT, ^pid, _} -> :error
+      after
+        100 -> :timeout
+      end
+
+    Map.put(results, input, reply)
   end
 
   def reliability_check(calculator, inputs) do
