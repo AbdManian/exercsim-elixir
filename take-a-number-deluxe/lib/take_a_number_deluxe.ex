f04d940777113cdf55aa3c06f2c370ceac79c020
AbdManian <abd.manian@gmail.com>
2022-05-25 01:02:46 +0430

    midnight commit (solution is not working)

--- take-a-number-deluxe/lib/take_a_number_deluxe.ex
+++ take-a-number-deluxe/lib/take_a_number_deluxe.ex
@@ -1,19 +1,20 @@
 defmodule TakeANumberDeluxe do
+  use GenServer
   # Client API
 
   @spec start_link(keyword()) :: {:ok, pid()} | {:error, atom()}
   def start_link(init_arg) do
-    # Please implement the start_link/1 function
+    GenServer.start_link(__MODULE__, init_arg)
   end
 
   @spec report_state(pid()) :: TakeANumberDeluxe.State.t()
   def report_state(machine) do
-    # Please implement the report_state/1 function
+   GenServer.call(machine, :report)
   end
 
   @spec queue_new_number(pid()) :: {:ok, integer()} | {:error, atom()}
   def queue_new_number(machine) do
-    # Please implement the queue_new_number/1 function
+    GenServer.call(machine, :insert_new_number)
   end
 
   @spec serve_next_queued_number(pid(), integer() | nil) :: {:ok, integer()} | {:error, atom()}
@@ -28,5 +29,26 @@ defmodule TakeANumberDeluxe do
 
   # Server callbacks
 
-  # Please implement the necessary callbacks
+  @impl GenServer
+  def init(init_args) do
+    case TakeANumberDeluxe.State.new(init_args[:min_number], init_args[:max_number]) do
+      {:ok, state} -> {:ok, state}
+      {:error, reson} -> {:stop, reson}
+    end
+  end
+
+  @impl GenServer
+  def handle_call(:report, _from, state) do
+    {:reply, state, state}
+  end
+
+
+  @impl GenServer
+  def handle_call(:insert_new_number, _from, state) do
+    case TakeANumberDeluxe.State.queue_new_number(state) do
+      {:ok, new_val, new_state} -> {:reply, {:ok, new_val}, new_state}
+      {:error, reson} -> {:stop, reson, state}
+    end
+
+  end
 end
