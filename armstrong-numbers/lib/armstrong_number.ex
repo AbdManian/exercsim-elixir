c769662cc00bbf283d800cd4615f8c40eea8d68b
AbdManian <abd.manian@gmail.com>
2022-05-26 01:12:16 +0430

    Solution submit

--- armstrong-numbers/lib/armstrong_number.ex
+++ armstrong-numbers/lib/armstrong_number.ex
@@ -5,5 +5,16 @@ defmodule ArmstrongNumber do
 
   @spec valid?(integer) :: boolean
   def valid?(number) do
+    digits =
+      number
+      |> Stream.unfold(fn
+        0 -> nil
+        n -> {rem(n, 10), div(n, 10)}
+      end)
+      |> Enum.to_list()
+
+    digits
+    |> Enum.map(fn x -> :math.pow(x, length(digits)) |> round() end)
+    |> Enum.sum() == number
   end
 end
