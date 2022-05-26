3d6365e7aa6223bc217ac382cb83ad81450f478b
Abd Manian <abd.manian@gmail.com>
2022-05-26 18:09:16 +0430

    Midnight commit

--- nth-prime/lib/prime.ex
+++ nth-prime/lib/prime.ex
@@ -5,4 +5,30 @@ defmodule Prime do
   @spec nth(non_neg_integer) :: non_neg_integer
   def nth(count) do
   end
+
+
+  def do_nth(primes, count) do
+    cond do
+      length(primes) == count -> primes
+      _ -> do_nth(next_prime(primes), )
+    end
+  end
+
+  when length(primes) == count, do: primes
+
+
+  def next_prime(prime_list, n) do
+    if isprime(prime_list, n) do
+      prime_list ++ [n]
+    else
+      next_prime(prime_list, n + 2)
+    end
+  end
+
+  defp isprime([], _), do: true
+  defp isprime([p|_], val) when rem(val, p) == 0, do: false
+  defp isprime([_|other], val), do: isprime(other, val)
+
+
+
 end
