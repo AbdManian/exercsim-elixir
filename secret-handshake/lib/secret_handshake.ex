c14829a414f80ad615d1470efeb2869ea1ee7308
Abd Manian <abd.manian@gmail.com>
2022-05-26 17:02:22 +0430

    Refactor based on community solutions

--- secret-handshake/lib/secret_handshake.ex
+++ secret-handshake/lib/secret_handshake.ex
@@ -15,16 +15,21 @@ defmodule SecretHandshake do
 
   10000 = Reverse the order of the operations in the secret handshake
   """
+
+  # Refactored based on Cohen-Carlisle's solution
+
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
     []
-    |> do_rule(code, 0b1000, &["jump" | &1])
-    |> do_rule(code, 0b0100, &["close your eyes" | &1])
-    |> do_rule(code, 0b0010, &["double blink" | &1])
-    |> do_rule(code, 0b0001, &["wink" | &1])
-    |> do_rule(code, 0b10000, &Enum.reverse/1)
+    |> do_rule(code &&& 0b1000, "jump")
+    |> do_rule(code &&& 0b0100, "close your eyes")
+    |> do_rule(code &&& 0b0010, "double blink")
+    |> do_rule(code &&& 0b0001, "wink")
+    |> do_rule(code &&& 0b10000, &Enum.reverse/1)
   end
 
-  defp do_rule(acc, value, mask, operation) when (value &&& mask) != 0, do: operation.(acc)
-  defp do_rule(acc, _, _, _), do: acc
+  defp do_rule(acc, mask_result, operation)
+  defp do_rule(acc, 0, _), do: acc
+  defp do_rule(acc, _, str) when is_binary(str), do: [str | acc]
+  defp do_rule(acc, _, op), do: op.(acc)
 end
