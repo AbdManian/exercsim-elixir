acfd91bc691e489bdb3a7a6216c7fab9840911df
AbdManian <abd.manian@gmail.com>
2022-05-27 01:29:29 +0430

    Initial solution

--- grains/lib/grains.ex
+++ grains/lib/grains.ex
@@ -3,7 +3,11 @@ defmodule Grains do
   Calculate two to the power of the input minus one.
   """
   @spec square(pos_integer()) :: {:ok, pos_integer()} | {:error, String.t()}
+  def square(number) when not is_integer(number) or number < 1 or number > 64,
+    do: {:error, "The requested square must be between 1 and 64 (inclusive)"}
+
   def square(number) do
+    {:ok, :math.pow(2, number - 1) |> round()}
   end
 
   @doc """
@@ -11,5 +15,6 @@ defmodule Grains do
   """
   @spec total :: {:ok, pos_integer()}
   def total do
+    {:ok, (:math.pow(2, 64) |> round()) - 1}
   end
 end
