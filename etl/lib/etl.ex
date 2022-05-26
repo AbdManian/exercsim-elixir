6cbeed409f1d046c4bfcd94f2c1e88f25b0fa92b
Abd Manian <abd.manian@gmail.com>
2022-05-26 12:50:48 +0430

    Refactor based on community solution

--- etl/lib/etl.ex
+++ etl/lib/etl.ex
@@ -7,18 +7,11 @@ defmodule ETL do
     iex> ETL.transform(%{1 => ["A", "E"], 2 => ["D", "G"]})
     %{"a" => 1, "d" => 2, "e" => 1, "g" => 2}
   """
+
   @spec transform(map) :: map
   def transform(input) do
-    input
-    |> Enum.reduce(%{}, fn
-      {k, v}, acc -> put_array_in_map(v, k, acc)
-    end)
-  end
-
-  @spec put_array_in_map(any, any, any) :: any
-  def put_array_in_map(data_list, key, map) do
-    data_list
-    |> Enum.map(&String.downcase/1)
-    |> Enum.reduce(map, fn x, acc -> Map.put(acc, x, key) end)
+    for {key, values} <- input, val <- values, into: %{} do
+      {String.downcase(val), key}
+    end
   end
 end
