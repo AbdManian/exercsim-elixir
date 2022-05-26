036fafdd325e0b831c57554fa99767a048b5cdf8
AbdManian <abd.manian@gmail.com>
2022-05-27 01:00:23 +0430

    Initial solution

--- nth-prime/lib/prime.ex
+++ nth-prime/lib/prime.ex
@@ -3,19 +3,24 @@ defmodule Prime do
   Generates the nth prime.
   """
   @spec nth(non_neg_integer) :: non_neg_integer
+
+  def nth(count) when count < 1, do: raise(ArgumentError)
+
   def nth(count) do
+    do_nth([2, 3], count)
+    |> List.last()
   end
 
+  def do_nth(_, 1), do: [2]
+
+  def do_nth(primes, count) when length(primes) == count, do: primes
 
   def do_nth(primes, count) do
-    cond do
-      length(primes) == count -> primes
-      _ -> do_nth(next_prime(primes), )
-    end
+    start = List.last(primes)
+
+    next_prime(primes, start + 2)
+    |> do_nth(count)
   end
-
-  when length(primes) == count, do: primes
-
 
   def next_prime(prime_list, n) do
     if isprime(prime_list, n) do
@@ -26,9 +31,6 @@ defmodule Prime do
   end
 
   defp isprime([], _), do: true
-  defp isprime([p|_], val) when rem(val, p) == 0, do: false
-  defp isprime([_|other], val), do: isprime(other, val)
-
-
-
+  defp isprime([p | _], val) when rem(val, p) == 0, do: false
+  defp isprime([_ | other], val), do: isprime(other, val)
 end
