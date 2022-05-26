eb19761ce14e045bad92517fcc13c1d8cf02463d
Abd Manian <abd.manian@gmail.com>
2022-05-26 16:51:40 +0430

    Initial import

--- secret-handshake/lib/secret_handshake.ex
+++ secret-handshake/lib/secret_handshake.ex
@@ -1,4 +1,6 @@
 defmodule SecretHandshake do
+  use Bitwise
+
   @doc """
   Determine the actions of a secret handshake based on the binary
   representation of the given `code`.
@@ -15,5 +17,14 @@ defmodule SecretHandshake do
   """
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
+    []
+    |> do_rule(code, 0b1000, &["jump" | &1])
+    |> do_rule(code, 0b0100, &["close your eyes" | &1])
+    |> do_rule(code, 0b0010, &["double blink" | &1])
+    |> do_rule(code, 0b0001, &["wink" | &1])
+    |> do_rule(code, 0b10000, &Enum.reverse/1)
   end
+
+  defp do_rule(acc, value, mask, operation) when (value &&& mask) != 0, do: operation.(acc)
+  defp do_rule(acc, _, _, _), do: acc
 end
