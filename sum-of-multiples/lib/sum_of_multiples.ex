366c4073577b7cda8a330dc24eec8120bd56e01b
Abd Manian <abd.manian@gmail.com>
2022-05-28 19:25:18 +0430

    Initial solution

--- sum-of-multiples/lib/sum_of_multiples.ex
+++ sum-of-multiples/lib/sum_of_multiples.ex
@@ -4,5 +4,12 @@ defmodule SumOfMultiples do
   """
   @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
   def to(limit, factors) do
+    1..(limit - 1)
+    |> Enum.filter(&mul_of_factors(&1, factors))
+    |> Enum.sum()
   end
+
+  defp mul_of_factors(_, []), do: false
+  defp mul_of_factors(value, [f | _]) when rem(value, f) == 0, do: true
+  defp mul_of_factors(value, [_ | rest]), do: mul_of_factors(value, rest)
 end
