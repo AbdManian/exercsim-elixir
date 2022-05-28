91e5ef50b03e4d47b970f7fd93752941bfc495a7
AbdManian <abd.manian@gmail.com>
2022-05-28 23:54:46 +0430

    Initial solution

--- proverb/lib/proverb.ex
+++ proverb/lib/proverb.ex
@@ -3,6 +3,18 @@ defmodule Proverb do
   Generate a proverb from a list of strings.
   """
   @spec recite(strings :: [String.t()]) :: String.t()
+  def recite([]), do: ""
+
   def recite(strings) do
+    (strings
+     |> Enum.chunk_every(2, 1, :discard)
+     |> Enum.map(&proverb/1)
+     |> Enum.join()) <>
+      proverb(Enum.at(strings, 0))
   end
+
+  defp proverb([word1, word2] = x) when is_list(x),
+    do: "For want of a #{word1} the #{word2} was lost.\n"
+
+  defp proverb(word), do: "And all for the want of a #{word}.\n"
 end
