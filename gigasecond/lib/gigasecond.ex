5b322e95786ff9a43739d27cc20226fcdf5ac65f
AbdManian <abd.manian@gmail.com>
2022-05-30 00:01:53 +0430

    Initial solution

--- gigasecond/lib/gigasecond.ex
+++ gigasecond/lib/gigasecond.ex
@@ -5,5 +5,10 @@ defmodule Gigasecond do
   @spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) ::
           {{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}
   def from({{year, month, day}, {hours, minutes, seconds}}) do
+    dt =
+      NaiveDateTime.new!(year, month, day, hours, minutes, seconds)
+      |> NaiveDateTime.add(1_000_000_000)
+
+    {{dt.year, dt.month, dt.day}, {dt.hour, dt.minute, dt.second}}
   end
 end
